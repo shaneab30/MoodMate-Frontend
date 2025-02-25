bdc2995694aba78643457734b8e8eaf74165d526
Shane Baskara <shaneardyanto@gmail.com>
2025-02-25 17:02:01 +0700

    update mood checker frontend

--- app/mood-checker/page.tsx
+++ app/mood-checker/page.tsx
@@ -3,6 +3,7 @@ import { Button, styled } from "@mui/material";
 import { Component, FunctionComponent, use, useEffect, useState } from "react";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Image from 'next/image';
+import styles from "./page.module.css";
 
 interface MoodCheckerProps {
 
@@ -31,45 +32,57 @@ const MoodChecker: FunctionComponent<MoodCheckerProps> = () => {
     }, []);
 
 
-    return (<div>
-        <h1 style={{ textAlign: "center", color: "black" }}>Mood Checker</h1>
+    return (<>
+        <div className={styles.container}>
+            <div className={styles.headerProfile}>
+                <h1 style={{ textAlign: "center", color: "black" }}>Mood Checker</h1>
+            </div>
 
-        <Button
-            component="label"
-            role={undefined}
-            variant="contained"
-            tabIndex={-1}
-            startIcon={<CloudUploadIcon />}
-        >
-            Upload Image
-            <VisuallyHiddenInput
-                type="file"
-                onChange={async (event) => {
-                    setImage(event.target.files![0]);
-                    console.log(event.target.files)
-                    const url = "http://127.0.0.1:5000/predict"
-                    const formData = new FormData();
-                    formData.append('file', event.target.files![0]);
-                    const response = await fetch(url, {
-                        headers: {
-                            'Accept': "application/json, text/plain, */*",
-                            'Content-Type': "application/json;charset=utf-8"
-                        },
-                        method: "POST",
-                        body: formData
-                    });
-                    const data = await response.json();
-                    console.log(data);
-                }}
-                // multiple
-                accept="image/*"
-            />
-        </Button>
+            <div className={styles.image}>
+                {image && <Image src={URL.createObjectURL(image)} width={500} height={500} alt="uploaded image"></Image>}
+            </div>
 
-        {/* {image && <img src={URL.createObjectURL(image)} alt="uploaded image" style={{ width: "100%", height: "auto" }}></img>} */}
-        {image && <Image src={URL.createObjectURL(image)} width={500} height={500} alt="uploaded image"></Image>}
-       
-    </div>);
+            <div className={styles.upload}>
+
+
+
+
+                <Button
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                >
+                    Upload Image
+                    <VisuallyHiddenInput
+                        type="file"
+                        onChange={async (event) => {
+                            setImage(event.target.files![0]);
+                            console.log(event.target.files)
+                            const url = "http://127.0.0.1:5000/upload"
+                            const formData = new FormData();
+                            formData.append('file', event.target.files![0]);
+                            const response = await fetch(url, {
+                                method: "POST",
+                                body: formData
+                            });
+
+                            const data = await response.json();
+                            console.log(data);
+                        }}
+                        // multiple
+                        accept="image/*"
+                    />
+                </Button>
+
+
+            </div>
+
+
+        </div>
+
+    </>);
 }
 
 export default MoodChecker;
